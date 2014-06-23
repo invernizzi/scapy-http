64b67f5b160c65fc3b10efa86280fbf9f930137a
Luca Invernizzi <Invernizzi.l@gmail.com>
2014-06-23 12:42:18 -0700

    Download URL updated

--- setup.py
+++ setup.py
@@ -31,6 +31,6 @@ setup(
     author=['Luca Invernizzi, Steeve Barbeau'],
     author_email=['invernizzi.l@gmail.com'],
     url='https://github.com/invernizzi/scapy-http',
-    download_url='https://github.com/invernizzi/scapy-http/tarball/1.0',
+    download_url='https://github.com/invernizzi/scapy-http/tarball/1.1',
     keywords=['http', 'scapy', 'newtork', 'dissect', 'packets']
 )
